714c979bab3d05223729a3c49ebd33b9d020eebe
Trevor Yovaish <tyovaish13@gmail.com>
2017-03-29 06:33:57 -0400

    Thread conditions?

--- src/Model/Tile/TileBuilder.java
+++ src/Model/Tile/TileBuilder.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 public class TileBuilder {
    public static Tile createTile(FeatureType featureType,TileEdge[] tileEdges, TileOrientation orientation){
       int numberOfRivers=0;
+      TileEdge [] copyTileEdges =new TileEdge[6];
       for(int i=0;i<tileEdges.length;i++){
          if(tileEdges[i]==null){
             tileEdges[i]=new TileEdge();
@@ -21,7 +22,12 @@ public class TileBuilder {
             numberOfRivers++;
          }
       }
+      for(int i=0;i<6;i++){
+         copyTileEdges[i]=new TileEdge();
+         copyTileEdges[i].setFeatureType(tileEdges[i].getFeatureType());
+      }
+     TileOrientation  copyOrientation=new TileOrientation(orientation.getRotations());
 
-      return new Tile(featureType,tileEdges,orientation,numberOfRivers);
+      return new Tile(featureType,copyTileEdges,copyOrientation,numberOfRivers);
    }
 }
